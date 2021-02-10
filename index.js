367eb9fef521c4d42d7caca7813f646aec9daf7d
MAHIN <prajapatimahin@gmail.com>
2021-02-10 18:35:40 +0530

    Preparation for deployment

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const server = express()
+const bodyParser = require('body-parser');
+const db = require('./db').connect()
+
+const portfolioRoutes = require('./routes/portfolios')
+const blogRoutes = require('./routes/blogs')
+
+runServer = async () => {
+    await (db)
+    server.use(bodyParser.json());
+    server.use('/api/v1/portfolios', portfolioRoutes)
+    server.use('/api/v1/blogs', blogRoutes)
+
+    const PORT = parseInt(process.env.PORT, 10) || 3001
+    server.listen(PORT, (err) => {
+        if (err) console.log(err)
+        console.log('Server ready on port', PORT)
+    })
+}
+
+runServer()
+
